ef40cd57a1c9e316926c31e14d807737decd47e4
Jan Kosek <smashercz1@gmail.com>
2018-10-04 22:19:46 +0200

    loading pics from imgur

--- public/script.js
+++ public/script.js
@@ -1,47 +1,60 @@
 new Vue({
-  el: '#app',
-  data: {
-    total: 0,
-    items: [
-      {id:1, title: 'Item 1', price:5.99},
-      {id:2, title: 'Item 2', price:5.99},
-      {id:3, title: 'Item 3', price:5.99},
-    ],
-    cart: [],
-  },
-  methods:{
-    addItem(index){
-      var item = this.items[index];
-      var cart_index = this.cart.findIndex(x=>x.id == item.id);
-      if(cart_index === -1){
-        this.cart.push({
-          id: item.id,
-          title: item.title,
-          price: 5.99,
-          qty: 1,
-        });
-      }else{
-        this.cart[cart_index].qty++ 
-      }
-      this.total += item.price;
+    el: '#app',
+    data: {
+        total: 0,
+        items: [],
+        cart: [],
+        searchbar: 'anime',
+        last_search: '',
+        loading: false,
+        price: 9.99,
     },
-    inc(item){
-      item.qty++;
-      this.total += item.price;
+    mounted() {
+      this.onSubmit();  
     },
-    dec(item){
-      if(item.qty-1 == 0){
-        var cart_index = this.cart.findIndex(x=>x.id == item.id);
-        this.cart.splice(cart_index, 1);
-      }else{
-        item.qty--;
-      }
-      this.total -= item.price;
+    methods:{
+        addItem(index){
+            var item = this.items[index];
+            var cart_index = this.cart.findIndex(x=>x.id == item.id);
+            if(cart_index === -1){
+                this.cart.push({
+                id: item.id,
+                title: item.title,
+                qty: 1,
+                });
+            }else{
+                this.cart[cart_index].qty++ 
+            }
+            this.total += item.price;
+        },
+        inc(item){
+            item.qty++;
+            this.total += item.price;
+        },
+        dec(item){
+            if(item.qty-1 == 0){
+                var cart_index = this.cart.findIndex(x=>x.id == item.id);
+                this.cart.splice(cart_index, 1);
+            }else{
+                item.qty--;
+            }
+            this.total -= item.price;
+        },
+        onSubmit(){
+            this.items = [];
+            this.loading = true;
+            this.$http.get('/search/'.concat(this.searchbar)).then(
+                (res)=>{
+                    this.last_search = this.searchbar;
+                    this.items = res.data;
+                    this.loading = false;
+                }
+            );
+        }
+    },
+    filters:{
+        currency(price){
+            return '$'+price.toFixed(2);
+        }
     }
-  },
-  filters:{
-    currency(price){
-      return '$'+price.toFixed(2);
-    }
-  }
 });
